39e606db66551a3950a88d5da7041322741a3634
I am Flin Sky <76969354+FlinSky2@users.noreply.github.com>
2021-01-12 19:49:25 +0700

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,92 +1,87 @@
                                           
 const help = (prefix) => {
         return `
-*Flin sky* | *whatsapp Bot*
+ ════ _*FLIN SKY BOT *_ ════
 
 _*Flin Sky info*_
 
-+ *${prefix}owner* *info owner*
-+ *${prefix}donasi* *info donasi*
-+ *${prefix}info* *flin sky info*
-+ *${prefix}botstat* *botstat*
-+ *${prefix}bug* *report bug to owner*
++ *${prefix}owner* ```info owner```
++ *${prefix}donasi* ```info donasi```
++ *${prefix}info* ```flin sky info```
++ *${prefix}botstat* ```botstat```
++ *${prefix}bug* ```report bug to owner```
 
 _*flin sky maker*_
 
-+ *${prefix}ocr* *image to text*
-+ *${prefix}sticker* *image to sticker*
-+ *${prefix}tts* *text to audio*
-+ *${prefix}toimg* *sticker to image*
-+ *${prefix}phlogo* *pornHub logo*
-+ *${prefix}quotemaker* *quotes maker*
-+ *${prefix}ninjalogo* *name ninja*
-+ *${prefix}glitch* *name glitch*
-+ *${prefix}text3d* *logo 3D*
++ *${prefix}sticker* ```image to sticker```
++ *${prefix}tts* ```text to audio```
++ *${prefix}toimg* ```sticker to image```
++ *${prefix}phlogo* ```pornHub logo```
++ *${prefix}quotemaker* ```quotes maker```
++ *${prefix}text3d* ```logo 3D```
 
 _*kerang ajaib*_
 
-+ *${prefix}kapankah* *optional*
-+ *${prefix}apakah* *optional*
-+ *${prefix}bisakah* *optional*
-+ *${prefix}rate* *optional*
++ *${prefix}kapankah* ```optional```
++ *${prefix}apakah* ```optional```
++ *${prefix}bisakah* ```optional```
++ *${prefix}rate* ```optional```
 
 _*flin sky fun menu*_
 
-+ *${prefix}meme* *random meme*
-+ *${prefix}memeindo* *random meme indo*
-+ *${prefix}hilih* *hilih text*
-+ *${prefix}truth* *random truth*
-+ *${prefix}dare* *random dare*
-+ *${prefix}bucin* *random bucin*
-+ *${prefix}persengay* *rate gay*
++ *${prefix}meme* ```random meme```
++ *${prefix}memeindo* ```random meme indo```
++ *${prefix}hilih* ```hilih text```
++ *${prefix}truth* ```random truth```
++ *${prefix}dare* ```random dare```
++ *${prefix}bucin* ```random bucin```
++ *${prefix}persengay* ```rate gay```
 
 
 _*flin sky media*_
 
-+ *${prefix}tiktokstalk* *tik tok stalking*
-+ *${prefix}ytmp4* *dowloader youtube*
++ *${prefix}tiktokstalk* ```tik tok stalking```
++ *${prefix}ytmp4* ```dowloader youtube```
 
 _*flin sky other menu*_
 
-+ *${prefix}ping* *ping Bot*
-+ *${prefix}shorturl* *get your link*
-+ *${prefix}fototiktok* *tik tok random photo*
-+ *${prefix}map* *maps contoh map kalimantan*
-+ *${prefix}kbbi* *kamus besar bahasa indonesia*
-+ *${prefix}artinama* *artinama (rey)
-+ *${prefix}infogempa* *info gempa terkini*
-+ *${prefix}quotes* *random quotes
-+ *${prefix}lirik* *quoery lirik lagu*
-+ *${prefix}chord* *quoery chord guitar*
-+ *${prefix}blocklist* *list block*
-+ *${prefix}hidetag* *hidetag [text]*
-+ *${prefix}ssweb* *screen web*
-+ *${prefix}anjing* *random gambar anjing*
++ *${prefix}shorturl* ```get your link```
++ *${prefix}fototiktok* ```tik tok random photo```
++ *${prefix}map* ```maps contoh map kalimantan```
++ *${prefix}kbbi* ```kamus besar bahasa indonesia```
++ *${prefix}artinama* ```artinama (rey)```
++ *${prefix}infogempa* ```info gempa terkini```
++ *${prefix}quotes* ```random quotes```
++ *${prefix}lirik* ```quoery lirik lagu```
++ *${prefix}chord* ```quoery chord guitar```
++ *${prefix}blocklist* ```list block```
++ *${prefix}hidetag* ```hidetag [text]```
++ *${prefix}ssweb* ```screen web```
++ *${prefix}anjing* ```random gambar anjing```
 
 _*flin sky group menu*_
 
-+ *${prefix}add* [628xxx] *add partisipan*
-+ *${prefix}group*  *group tutup/buka*
-+ *${prefix}demote* *tag admin*
-+ *${prefix}promote* *tag member*
-+ *${prefix}kick*   *tag member*
-+ *${prefix}kudeta* *ambil alih*
-+ *${prefix}listadmin* *admin list*
-+ *${prefix}infogc* *group info*
-+ *${prefix}setdesc *set deskripsi group*
-+ *${prefix}welcome* [1/0]
++ *${prefix}add* ```[628xxx]add partisipan```
++ *${prefix}group*  ```group tutup/buka```
++ *${prefix}demote* ```tag admin```
++ *${prefix}promote* *tag member```
++ *${prefix}kick*   ```tag member```
++ *${prefix}kudeta* ```ambil alih```
++ *${prefix}listadmin* ```admin list```
++ *${prefix}infogc* ```group info```
++ *${prefix}setdesc ```set deskripsi group```
++ *${prefix}welcome* ```welcome [1/0]```
 
 _*owner menu*_
 
-+ *${prefix}clearall*
-+ *${prefix}block*
-+ *${prefix}unblock*
-+ *${prefix}bc*
-+ *${prefix}leave*
-+ *${prefix}clone*
-+ *${prefix}setprefix*
++ *${prefix}clearall* ```clear all chats```
++ *${prefix}block* ```bloked partisipans```
++ *${prefix}unblock* ```unblok partisipans```
++ *${prefix}bc* ```promosi```
++ *${prefix}clone* ```set profile```
++ *${prefix}setprefix* ```set prefix```
 
-📣Have fun
+ ════ _*POWERED BY TN.RAR*_ ════
 
 `
 }
