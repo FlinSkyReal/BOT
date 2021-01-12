63040ae250d3c2bd7bd2afe253d996fb753505ad
I am Flin Sky <76969354+FlinSkyReal@users.noreply.github.com>
2021-01-12 20:36:07 +0700

    Update help.js

--- help.js
+++ help.js
@@ -5,89 +5,88 @@ const help = (prefix) => {
 
 _*Ｉｎｆｏｒｍａｔｉｏｎ*_
 
-+ *Bot name* = *FLIN sky*
++ *Bot name* = *FLIN Sky*
 + *Author*   = *Tn.RAR*
 + *Owner*    = *Rey sekha* https://wa.me/6281246460730
 + *Version*  = *v1.3*
-+ *run on*   = *Termux*
 
 _*Ｆｌｉｎ Ｓｋｙ ｉｎｆｏ*_
 
-+ *${prefix}owner* ```info owner```
-+ *${prefix}donasi* ```info donasi```
-+ *${prefix}info* ```flin sky info```
-+ *${prefix}botstat* ```botstat```
-+ *${prefix}bug* ```report bug to owner```
++ *${prefix}owner*
++ *${prefix}donasi* 
++ *${prefix}info* 
++ *${prefix}botstat* 
++ *${prefix}bug* 
 
 _*ｆｌｉｎ ｓｋｙ ｍａｋｅｒ*_
 
-+ *${prefix}sticker* ```image to sticker```
-+ *${prefix}tts* ```text to audio```
-+ *${prefix}toimg* ```sticker to image```
-+ *${prefix}phlogo* ```pornHub logo```
-+ *${prefix}quotemaker* ```quotes maker```
-+ *${prefix}text3d* ```logo 3D```
++ *${prefix}sticker* 
++ *${prefix}tts* 
++ *${prefix}toimg* 
++ *${prefix}phlogo* 
++ *${prefix}quotemaker* 
++ *${prefix}text3d* 
 
 _*ｋｅｒａｎｇ ａｊａｉｂ*_
 
-+ *${prefix}kapankah* ```optional```
-+ *${prefix}apakah* ```optional```
-+ *${prefix}bisakah* ```optional```
-+ *${prefix}rate* ```optional```
++ *${prefix}kapankah* 
++ *${prefix}apakah* 
++ *${prefix}bisakah* 
++ *${prefix}rate* 
 
 _*ｆｌｉｎ ｓｋｙ ｆｕｎ ｍｅｎｕ*_
 
-+ *${prefix}meme* ```random meme```
-+ *${prefix}memeindo* ```random meme indo```
-+ *${prefix}hilih* ```hilih text```
-+ *${prefix}truth* ```random truth```
-+ *${prefix}dare* ```random dare```
-+ *${prefix}bucin* ```random bucin```
-+ *${prefix}persengay* ```rate gay```
++ *${prefix}meme* 
++ *${prefix}memeindo* 
++ *${prefix}hilih* 
++ *${prefix}truth*
++ *${prefix}dare* 
++ *${prefix}bucin* 
++ *${prefix}persengay* 
 
 
 _*ｆｌｉｎ ｓｋｙ ｍｅｄｉａ*_
 
-+ *${prefix}tiktokstalk* ```tik tok stalking```
-+ *${prefix}ytmp4* ```dowloader youtube```
++ *${prefix}tiktokstalk* 
++ *${prefix}ytmp4* 
 
 _*ｆｌｉｎ ｓｋｙ ｏｔｈｅｒ ｍｅｎｕ*_
 
-+ *${prefix}shorturl* ```get your link```
-+ *${prefix}fototiktok* ```tik tok random photo```
-+ *${prefix}map* ```maps contoh map kalimantan```
-+ *${prefix}kbbi* ```kamus besar bahasa indonesia```
-+ *${prefix}artinama* ```artinama (rey)```
-+ *${prefix}infogempa* ```info gempa terkini```
-+ *${prefix}quotes* ```random quotes```
-+ *${prefix}lirik* ```quoery lirik lagu```
-+ *${prefix}chord* ```quoery chord guitar```
-+ *${prefix}blocklist* ```list block```
-+ *${prefix}hidetag* ```hidetag [text]```
-+ *${prefix}ssweb* ```screen web```
-+ *${prefix}anjing* ```random gambar anjing```
++ *${prefix}shorturl* 
++ *${prefix}fototiktok* 
++ *${prefix}map*
++ *${prefix}kbbi* 
++ *${prefix}artinama*
++ *${prefix}infogempa* 
++ *${prefix}quotes*
++ *${prefix}lirik* 
++ *${prefix}chord* 
++ *${prefix}blocklist* 
++ *${prefix}hidetag* 
++ *${prefix}ssweb* 
++ *${prefix}anjing*
 
 _*ｆｌｉｎ ｓｋｙ ｇｒｏｕｐ ｍｅｎｕ*_
 
-+ *${prefix}add* ```[628xxx]add partisipan```
-+ *${prefix}group*  ```group tutup/buka```
-+ *${prefix}demote* ```tag admin```
-+ *${prefix}promote* *tag member```
-+ *${prefix}kick*   ```tag member```
-+ *${prefix}kudeta* ```ambil alih```
-+ *${prefix}listadmin* ```admin list```
-+ *${prefix}infogc* ```group info```
-+ *${prefix}setdesc ```set deskripsi group```
-+ *${prefix}welcome* ```welcome [1/0]```
++ *${prefix}add* *[628xxx]add partisipan*
++ *${prefix}group*  *group tutup/buka*
++ *${prefix}demote* *tag admin*
++ *${prefix}promote* *tag member*
++ *${prefix}kick*   tag member
++ *${prefix}kudeta* 
++ *${prefix}listadmin* 
++ *${prefix}infogc* group info
++ *${prefix}setdesc 
++ *${prefix}welcome* *welcome [1/0]*
 
 _*ｏｗｎｅｒ ｍｅｎｕ*_
 
-+ *${prefix}clearall* ```clear all chats```
-+ *${prefix}block* ```bloked partisipans```
-+ *${prefix}unblock* ```unblok partisipans```
-+ *${prefix}bc* ```promosi```
-+ *${prefix}clone* ```set profile```
-+ *${prefix}setprefix* ```set prefix```
++ *${prefix}clearall* 
++ *${prefix}block*
++ *${prefix}unblock* 
++ *${prefix}bc* 
++ *${prefix}clone* 
++ *${prefix}setprefix* 
 
  ════ _*POWERED BY TN.RAR*_ ════
 
