dfd4aa49c183c46540e29e873c836ed08525c8e3
I am Flin Sky <76969354+FlinSkyReal@users.noreply.github.com>
2021-01-13 14:04:37 +0700

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,94 +1,96 @@
                                           
 const help = (prefix) => {
         return `
- ════ _*FLIN SKY BOT *_ ════
+ 
+ *════ FLIN SKY BOT ════*
 
 _*Ｉｎｆｏｒｍａｔｉｏｎ*_
 
 + *Bot name* = *FLIN Sky*
 + *Author*   = *Tn.RAR*
-+ *Owner*    = *Rey sekha* https://wa.me/6281246460730
++ *Owner*    = *Rey sekha*
 + *Version*  = *v1.3*
 
 _*Ｆｌｉｎ Ｓｋｙ ｉｎｆｏ*_
 
-+ *${prefix}owner*
-+ *${prefix}donasi* 
-+ *${prefix}info* 
-+ *${prefix}botstat* 
-+ *${prefix}bug* 
++ *.owner*
++ *.donasi* 
++ *.info* 
++ *.botstat* 
++ *.bug* 
 
 _*ｆｌｉｎ ｓｋｙ ｍａｋｅｒ*_
 
-+ *${prefix}sticker* 
-+ *${prefix}tts* 
-+ *${prefix}toimg* 
-+ *${prefix}phlogo* 
-+ *${prefix}quotemaker* 
-+ *${prefix}text3d* 
++ *.sticker* 
++ *.tts* 
++ *.toimg* 
++ *.phlogo* 
++ *.quotemaker* 
++ *.text3d* 
 
 _*ｋｅｒａｎｇ ａｊａｉｂ*_
 
-+ *${prefix}kapankah* 
-+ *${prefix}apakah* 
-+ *${prefix}bisakah* 
-+ *${prefix}rate* 
++ *.kapankah* 
++ *.apakah* 
++ *.bisakah* 
++ *.rate* 
 
 _*ｆｌｉｎ ｓｋｙ ｆｕｎ ｍｅｎｕ*_
 
-+ *${prefix}meme* 
-+ *${prefix}memeindo* 
-+ *${prefix}hilih* 
-+ *${prefix}truth*
-+ *${prefix}dare* 
-+ *${prefix}bucin* 
-+ *${prefix}persengay* 
++ *.meme* 
++ *.memeindo* 
++ *.hilih* 
++ *.truth*
++ *.dare* 
++ *.bucin* 
++ *.persengay* 
 
 
 _*ｆｌｉｎ ｓｋｙ ｍｅｄｉａ*_
 
-+ *${prefix}tiktokstalk* 
-+ *${prefix}ytmp4* 
++ *.tiktokstalk* 
++ *.ytmp4* 
 
 _*ｆｌｉｎ ｓｋｙ ｏｔｈｅｒ ｍｅｎｕ*_
 
-+ *${prefix}shorturl* 
-+ *${prefix}fototiktok* 
-+ *${prefix}map*
-+ *${prefix}kbbi* 
-+ *${prefix}artinama*
-+ *${prefix}infogempa* 
-+ *${prefix}quotes*
-+ *${prefix}lirik* 
-+ *${prefix}chord* 
-+ *${prefix}blocklist* 
-+ *${prefix}hidetag* 
-+ *${prefix}ssweb* 
-+ *${prefix}anjing*
++ *.shorturl* 
++ *.fototiktok* 
++ *.map*
++ *.kbbi* 
++ *.artinama*
++ *.infogempa* 
++ *.quotes*
++ *.lirik* 
++ *.chord* 
++ *.blocklist* 
++ *.hidetag* 
++ *.ssweb* 
++ *.anjing*
 
 _*ｆｌｉｎ ｓｋｙ ｇｒｏｕｐ ｍｅｎｕ*_
 
-+ *${prefix}add* *[628xxx]add partisipan*
-+ *${prefix}group*  *group tutup/buka*
-+ *${prefix}demote* *tag admin*
-+ *${prefix}promote* *tag member*
-+ *${prefix}kick*   tag member
-+ *${prefix}kudeta* 
-+ *${prefix}listadmin* 
-+ *${prefix}infogc* group info
-+ *${prefix}setdesc 
-+ *${prefix}welcome* *welcome [1/0]*
++ *.add* *[628xxx]add partisipan*
++ *.group*  *group tutup/buka*
++ *.demote* *tag admin*
++ *.promote* *tag member*
++ *.kick*   tag member
++ *.kudeta* 
++ *.listadmin* 
++ *.infogc* group info
++ *.setdesc*
++ *.welcome* *welcome [1/0]*
 
 _*ｏｗｎｅｒ ｍｅｎｕ*_
 
-+ *${prefix}clearall* 
-+ *${prefix}block*
-+ *${prefix}unblock* 
-+ *${prefix}bc* 
-+ *${prefix}clone* 
-+ *${prefix}setprefix* 
++ *.clearall* 
++ *.block*
++ *.unblock* 
++ *.bc* 
++ *.clone* 
++ *.setprefix* 
 
  ════ _*POWERED BY TN.RAR*_ ════
+
 
 `
 }
