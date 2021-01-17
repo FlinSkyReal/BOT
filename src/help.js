f58d6cb003c48e6704d17498d3dfb6bdae375838
I am Flin Sky <76969354+FlinSkyReal@users.noreply.github.com>
2021-01-17 14:13:35 +0700

    Update help.js

--- src/help.js
+++ src/help.js
@@ -4,80 +4,80 @@ const help = (prefix) => {
  
  *FLIN SKY BOT* || *By _sekhaa*
 
-🗒️ ```menu total``` : *56*
-🔎 ```status Menu``` : *false*
-💎 ```info limit user ``` : *unlimited*
-📍 ```Pause command``` : *5sec*
-❕ ```prefix``` : *. Titik*
+🗒️ *menu total* : *56*
+🔎 *status Menu* : *false*
+💎 *info limit user* : *unlimited only*
+📍 *Pause command* : *5sec*
+❕ *prefix* : *. Titik*
 
-📃```FLIN INFO```
-    ├ ```.owner``` *creator*
-    ├ ```.donasi``` *amal*
-    ├ ```.info``` *Flin info*
-    ├ ```.bug``` *report Bug*
+📃*FLIN INFO*
+    ├ *.owner* creator
+    ├ *.donasi* amal
+    ├ *.info* Flin info
+    ├ *.bug* report Bug
    
-📃```FLIN MAKER```
-    ├ ```.sticker```
-    ├ ```.tts``` 
-    ├ ```.phlogo```
-    ├ ```.quotemaker```
-    ├ ```.text3d```
+📃*FLIN MAKER*
+    ├ *.sticker* image to stiker
+    ├ *.tts* teks to sound/google sound
+    ├ *.phlogo* phlogo maker
+    ├ *.quotemaker* quotes maker
+    ├ *.text3d* tekx3d maker 
 
-📃```FLIN KERANG```
-    ├ ```.kapankah```
-    ├ ```.apakah```
-    ├ ```.bisakah```
-    ├ ```.rate```
+📃*FLIN KERANG*
+    ├ *.kapankah*
+    ├ *.apakah*
+    ├ *.bisakah*
+    ├ *.rate*
 
-📃```FLIN FUN```
-   ├ ```.meme```
-   ├ ```.memindo```
-   ├ ```.hilih```
-   ├ ```.truth```
-   ├ ```.dare```
-   ├ ```.bucin```
-   ├ ```.persengay```
+📃*FLIN FUN*
+   ├ *.meme*
+   ├ *.memindo*
+   ├ *.hilih*
+   ├ *.truth*
+   ├ *.dare*
+   ├ *.bucin*
+   ├ *.persengay*
 
-📃```FLIN MEDIA/DOWNLOAD```
-   ├ ```.tiktokstalk``` *@username*
-   ├ ```.ytvid``` *link YouTube max 5mb* 
+📃*FLIN MEDIA/DOWNLOAD*
+   ├ *.tiktokstalk* @username
+   ├ *.ytvid* link YouTube max 5mb
 
-📃```FLIN OTHERMENU```
-   ├ ```.shorturl``` *your url*
-   ├  ```.fototiktok```
-   ├ ```.map``` *daerah/kota*
-   ├ ```.kbbi``` *kbbi query* 
-   ├ ```.artinama``` *your name*
-   ├ ```.infogempa``` *BMKG INFO* 
-   ├ ```.quotes``` *random quotes*
-   ├ ```.lirik``` *query*
-   ├ ```.chord``` *guitar chord*
-   ├ ```.blocklist```
-   ├ ```.ssweb``` *url website*
-   ├ ```.anjing``` 
+📃*FLIN OTHERMENU*
+   ├ *.shorturl* your url
+   ├ *.fototiktok
+   ├ *.map* daerah/kota
+   ├ *.kbbi* kbbi query
+   ├ *.artinama* your name
+   ├ *.infogempa* BMKG INFO
+   ├ *.quotes* random quotes
+   ├ *.lirik* query
+   ├ *.chord* guitar chord
+   ├ *.blocklist*
+   ├ *.ssweb* url website
+   ├ *.anjing* 
 
-📃```FLIN GROUP```
-   ├ ```.hidetag```
-   ├ ```.add``` *6283xxxx*
-   ├ ```.kick``` *@tag members*
-   ├ ```.promote``` *@tag members*
-   ├ ```.demote``` *@tag admins*
-   ├ ```.tagme```
-   ├ ```.mention```
-   ├ ```.setname``` *set group name*
-   ├ ```.setdesc``` *set deskripsi group*
-   ├ ```.bar```
-   ├ ```.kudeta```
-   ├ ```.listadmin``` *admin list*
-   ├ ```.infogc``` *group info*
+📃*FLIN GROUP*
+   ├ *.hidetag*
+   ├ *.add* 6283xxxx
+   ├ *.kick* @tag members
+   ├ *.promote* @tag members
+   ├ *.demote* @tag admins
+   ├ *.tagme*
+   ├ *.mention*
+   ├ *.setname* set group name
+   ├ *.setdesc* set deskripsi group
+   ├ *.bar*
+   ├ *.kudeta*
+   ├ *.listadmin* admin list
+   ├ *.infogc* group info
 
-📃```FLIN OWNER```
-   ├ ```.block```
-   ├ ```.unblock```
-   ├ ```.bc```
-   ├ ```.clone```
-   ├ ```.setpp```
-   ├ ```.setprefix```
+📃*FLIN OWNER*
+   ├ *.block*
+   ├ *.unblock*
+   ├ *.bc*
+   ├ *.clone*
+   ├ *.setpp*
+   ├ *.setprefix*
 
 *copyright* *by* _*@_sekhaa*_ || *2021*
 
